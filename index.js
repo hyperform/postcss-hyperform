5f95a0cfe02bfe5b4887fec9301a80a5a41d6e94
Manuel Strehl <m.strehl@kinetiqa.de>
2016-12-05 15:28:08 +0100

    add option to polyfill :required, :disabled et al.
    The polyfills are not truly equivalent unfortunately. See
    comment in index.js.

--- index.js
+++ index.js
@@ -4,7 +4,36 @@
 const postcss = require('postcss');
 
 
-function updateSelector(selector) {
+/**
+ * update the selector with the matching .hf-* classes
+ *
+ * If extended == true, also add some polyfills. Please note, that the
+ * polyfills _may_ be dangerous, because they might not have the scope
+ * you thought. Example:
+ *
+ *     *:enabled {
+ *       // match _input_ elements, that are not disabled
+ *     }
+ *
+ *     *:not([disabled]) {
+ *       // match _any_ element, also div and body and iframe, without
+ *       // a `disabled` attribute
+ *     }
+ *
+ * If you use these polyfills, please always make sure that you scope
+ * your selectors appropriately yourself.
+ */
+function updateSelector(selector, extended) {
+  if (extended) {
+    selector = selector
+      .replace(/:enabled\b/, ':not([disabled])')
+      .replace(/:disabled\b/, '[disabled]')
+      .replace(/:read-only\b/, '[readonly]')
+      .replace(/:read-write\b/, ':not([readonly])')
+      .replace(/:required\b/, '[required]')
+      .replace(/:optional\b/, ':not([required])')
+      ;
+  }
   return (selector
     .replace(/:user-(invalid|error)\b/g, '.hf-user-invalid')
     .replace(/:user-valid\b/g, '.hf-user-valid')
@@ -16,13 +45,16 @@ function updateSelector(selector) {
 }
 
 
-module.exports = postcss.plugin('postcss-hyperform', (options = {}) => {
+module.exports = postcss.plugin('postcss-hyperform', (options = {
+  /* extended polyfill is disabled by default due to above scoping issue */
+  extended: false,
+}) => {
   return root => {
     root.walkRules(rule => {
       rule.walkDecls(decl => {
         var userErrorPseudos = [];
         rule.selectors.forEach(function (selector) {
-          const new_selector = updateSelector(selector);
+          const new_selector = updateSelector(selector, !! options.extended);
           if (selector !== new_selector) {
             userErrorPseudos.push(new_selector);
           }
--- test.js
+++ test.js
@@ -5,10 +5,10 @@ import test    from 'ava';
 
 import plugin from './';
 
-function run(t, input, output, opts = { }) {
+function run(t, input, expected, opts = { }) {
   return postcss([ plugin(opts) ]).process(input)
            .then( result => {
-             t.deepEqual(result.css, output);
+             t.deepEqual(result.css, expected);
              t.deepEqual(result.warnings().length, 0);
            });
 }
@@ -23,14 +23,64 @@ test('base', t => {
 }
 .input:user-invalid {
   background: red;
+}
+:required {
+  content: "No extended behavior";
+}
+.input:user-invalid:out-of-range {
+  content: ":valid";
 }`,
 
 `.input:valid, .input.hf-valid {
   background: green;
 }
-.input:user-invalid, .input.hf-validated.hf-invalid {
+.input:user-invalid, .input.hf-user-invalid {
   background: red;
+}
+:required {
+  content: "No extended behavior";
+}
+.input:user-invalid:out-of-range, .input.hf-user-invalid.hf-out-of-range {
+  content: ":valid";
 }`
 
   );
+});
+
+test('extended', t => {
+  return run(t,
+
+`.input:valid {
+  background: green;
+}
+div:disabled {
+  opacity: 0;
+}
+:read-only, :optional :disabled {
+  color: gray;
+}
+:in-range:required {
+  content: ":required";
+}
+.input:user-invalid {
+  background: red;
+}`,
+
+`.input:valid, .input.hf-valid {
+  background: green;
+}
+div:disabled, div[disabled] {
+  opacity: 0;
+}
+:read-only, :optional :disabled, [readonly], :not([required]) [disabled] {
+  color: gray;
+}
+:in-range:required, .hf-in-range[required] {
+  content: ":required";
+}
+.input:user-invalid, .input.hf-user-invalid {
+  background: red;
+}`,
+
+{ extended: true });
 });
